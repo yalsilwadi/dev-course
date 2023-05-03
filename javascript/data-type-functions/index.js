3508fb1f86ce6af7babe9e89417f10848469deae
yalsilwadi <yasseralsilwadi@gmail.com>
2023-05-02 20:04:10 -0500

    added notes from class

---
+++ javascript/data-type-functions/index.js
@@ -0,0 +1,21 @@
+// var zooAnimals = ["Rhino", "Zebra", "Giraffe", "Owl"]
+
+// zooAnimals.forEach(animal => console.log(animal))
+
+// var favBands = ["The Beatles", "RHCP", "Green Day", "Led Zeppelin"]
+
+// favBands.push(window.prompt("Please enter your favorite band"));
+
+// favBands.forEach(band => console.log(band))
+
+// function prodOf4(num1, num2, num3, num4){
+//     return num1*num2*num3*num4;
+// }
+// console.log(prodOf4(1,2,3,4));
+
+// function hello(fname, mname, lname){
+//     alert("Hello " + fname + " " + mname + " " + lname)
+//     // console.log("Hello " + fname + " " + mname + " " + lname)
+// }
+
+// hello("Yasser", "Z", "Alsilwadi")
